74eb8c16cfc95f446ebe421d27440d758ae4f870
Juerg Schlienger <info@medeor.ch>
2025-07-02 17:54:36 +0200

    Add Express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello HRSE Backend");
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('HRSE Backend läuft');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
